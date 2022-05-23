975c327c22410774ec47aab856f6e2059a38b1a0
viet.nguyen <viet.nguyen@10.10.35.40>
2022-05-23 16:24:16 +0700

    updated

--- Scripts/New Test Case/Script1653296371161.groovy
+++ Scripts/New Test Case/Script1653296371161.groovy
@@ -23,7 +23,7 @@ WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
 
 WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/a_Make Appointment'))
 
-WebUI.setText(findTestObject('Object Repository/Page_CURA Healthcare Service/input_Username_username'), 'John Doe')
+WebUI.setText(findTestObject('Object Repository/Page_CURA Healthcare Service/input_Username_username'), 'John Do')
 
 WebUI.setEncryptedText(findTestObject('Object Repository/Page_CURA Healthcare Service/input_Password_password'), 'g3/DOGG74jC3Flrr3yH+3D/yKbOqqUNM')
 
